54ee71c3e14872b0a6d100b4cf431df11a057f98
Michael Zaverukha <zorkiz28@gmail.com>
2023-02-24 13:59:28 +0200

    dz23

---
+++ scripts/circle.js
@@ -0,0 +1,4 @@
+let radius = prompt ('Ваш радіус кола (см)');
+
+alert(`Ваша площа ${3.14*Math.pow(radius, 2)} (см^2)`);
+
---
+++ scripts/converter.js
@@ -0,0 +1,8 @@
+const euro = 0.95;
+const hryvna = 36.76;
+
+let convert = prompt ('Скільки у вас долларів?');
+
+alert (`Це буде ${convert*euro} євро`);
+
+alert (`Це буде ${convert*hryvna} гривень`);
